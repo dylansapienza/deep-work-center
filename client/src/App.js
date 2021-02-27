0691ababc52546da0ef6462b306baa9eb1d470d7
Dylan Sapienza <dylsapienza12@gmail.com>
2021-02-27 17:58:13 -0500

    Using Tailwind CSS

--- client/src/App.js
+++ client/src/App.js
@@ -21,10 +21,19 @@ function App(){
 
 
   return(
-    <div className="App">
-        <h1>Hello from the frontend!</h1>
-        <h1>{response.body}</h1>
+    <div class = "min-h screen flex items-center bg-purple-400">
+      <div class = "flex-1 max-w-4xl mx-auto p-10">
+        <div class = "bg-white rounded-lg shadow-xl">
+          <div class = "h-24">
+            <div class= "font-sans text-2xl antialiased text-center text-blue-700">
+              <h1>Hello from the frontend!</h1>
+              <h1>{response.body}</h1>
+            </div>
+          </div>
+        </div>
       </div>
+    </div>
+    
   );
 
 }
