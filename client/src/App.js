966b176d6eb719dd4638b5f4c6f1e7446a8c3293
Dylan Sapienza <dylsapienza12@gmail.com>
2021-03-07 12:02:42 -0500

    Formatting Continues

--- client/src/App.js
+++ client/src/App.js
@@ -21,17 +21,25 @@ function App(){
 
 
   return(
-    <div class = "min-h screen flex items-center bg-purple-400">
-      <div class = "flex-1 max-w-4xl mx-auto p-10">
-        <div class = "bg-white rounded-lg shadow-xl">
-          <div class = "h-24">
-            <div class= "font-sans text-2xl antialiased text-center text-blue-700">
-              <h1>Hello from the frontend!</h1>
-              <h1>{response.body}</h1>
+    <div class = "h-full bg-blue-200">
+      <div class = "flex-auto max-w-9xl mx-auto p-10">
+        <h2 class="text-3xl sm:text-5xl lg:text-6xl leading-none font-extrabold text-gray-900 tracking-tight mb-8">Deep Work Center</h2>
+          <div class = "grid justify-items-stretch gap-8 grid-cols-4">
+            <div class = "md:col-span-2 col-span-4">
+              <img class = "rounded-xl shadow-xl" src = "https://content.presspage.com/uploads/63/1920_aristotlequote.jpg?10000" style = {{maxWidth: '100%'}}></img>
             </div>
+            <div class = "md:col-span-2 col-span-4">
+              <div class = "bg-white rounded-xl shadow-xl">
+                <div class = "h-72"></div>
+              </div>
+              </div>
+            {/* task list */}
+            <div class = "justify-self-center col-span-4">
+              <img class = "rounded-xl shadow-xl" src = "https://content.presspage.com/uploads/63/1920_aristotlequote.jpg?10000" style = {{maxWidth: '100%'}}></img>
           </div>
+          </div>
+
         </div>
-      </div>
     </div>
     
   );
