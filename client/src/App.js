6010ad4a1ae43b472545c39d2fa54a12c0736255
Dylan Sapienza <dylsapienza12@gmail.com>
2021-03-07 12:24:11 -0500

    Beginning on Timer Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import './index.css';
 import axios from 'axios'
+import Timer from './components/Timer'
 
 
 
@@ -23,17 +24,13 @@ function App(){
   return(
     <div class = "h-full bg-blue-200">
       <div class = "flex-auto max-w-9xl mx-auto p-10">
-        <h2 class="text-3xl sm:text-5xl lg:text-6xl leading-none font-extrabold text-gray-900 tracking-tight mb-8">Deep Work Center</h2>
+        <h2 class="text-center text-3xl sm:text-5xl lg:text-6xl leading-none font-extrabold text-gray-900 tracking-tight mb-8">Deep Work Center</h2>
           <div class = "grid justify-items-stretch gap-8 grid-cols-4">
             <div class = "md:col-span-2 col-span-4">
               <img class = "rounded-xl shadow-xl" src = "https://content.presspage.com/uploads/63/1920_aristotlequote.jpg?10000" style = {{maxWidth: '100%'}}></img>
             </div>
-            <div class = "md:col-span-2 col-span-4">
-              <div class = "bg-white rounded-xl shadow-xl">
-                <div class = "h-72"></div>
-              </div>
-              </div>
-            {/* task list */}
+              <Timer />
+             {/* task list */}
             <div class = "justify-self-center col-span-4">
               <img class = "rounded-xl shadow-xl" src = "https://content.presspage.com/uploads/63/1920_aristotlequote.jpg?10000" style = {{maxWidth: '100%'}}></img>
           </div>
---
+++ client/src/components/Timer.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import '../index.css';
+
+function Timer(props){
+
+    const [startStop, setStartStop] = useState("Start")
+
+    function statusChange(){
+        if(startStop === "Start"){
+            setStartStop("Pause")
+        }
+        else{
+            setStartStop("Start")
+        }
+    }
+
+    return(
+        <div class = "md:col-span-2 col-span-4">
+            <div class = "bg-white rounded-xl shadow-xl">
+                <div class = "h-72">
+                    <h2 class="text-center text-3xl sm:text-3xl lg:text-4xl leading-normal font-extrabold text-gray-900 tracking-tight mb-8 ">Timer</h2>
+                    <h2 class="text-center text-5xl sm:text-5xl lg:text-6xl leading-normal font-extrabold text-gray-900 tracking-tight mb-8 ">15:00</h2>
+                    <div class = "flex justify-center">
+                        <button
+                            onClick={statusChange}
+                            type="button"
+                            class="border border-gray-700 bg-gray-700 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-gray-800 focus:outline-none focus:shadow-outline">
+                            {startStop}
+                        </button>
+                        <button
+                            type="button"
+                            class="border border-gray-200 bg-gray-200 text-gray-700 rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-gray-300 focus:outline-none focus:shadow-outline">
+                            Reset
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Timer;
